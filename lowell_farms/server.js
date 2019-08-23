6e7c3fbfe89e21ac079bf84c57b522b1b73ee434
sarahholden <sarahbeth.holden@gmail.com>
2019-08-23 12:47:49 -0700

    Updates to https

--- lowell_farms/server.js
+++ lowell_farms/server.js
@@ -35,12 +35,11 @@ if (!dev && cluster.isMaster) {
       // Enforce SSL & HSTS in production
       server.use(function(req, res, next) {
         const proto = req.headers['x-forwarded-proto'];
-        // if (proto === 'https') {
-          res.set({
-            'Strict-Transport-Security': 'max-age=31557600', // one-year
-          });
-          return next();
-        // }
+
+        res.set({
+          'Strict-Transport-Security': 'max-age=31557600', // one-year
+        });
+        return next();
         // res.redirect(`https://${req.headers.host}${req.url}`);
       });
     }
