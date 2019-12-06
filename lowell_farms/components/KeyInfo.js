a5e9d289b4e2be71585b68fb35bdff3644903be1
sarahholden <sarahbeth.holden@gmail.com>
2019-12-06 14:09:57 -0800

    Updates to main site

--- lowell_farms/components/KeyInfo.js
+++ lowell_farms/components/KeyInfo.js
@@ -7,30 +7,36 @@ class KeyInfo extends Component {
   render() {
     const { pageData } = this.props;
 
-    var locationHeadingHtml = RichText.render(pageData.location_heading, linkResolver);
+    var locationHeadingText = RichText.asText(pageData.location_heading, linkResolver);
     var addressHtml = RichText.render(pageData.address, linkResolver);
     var phoneHtml = RichText.asText(pageData.phone, linkResolver);
     var emailAddressHtml = RichText.asText(pageData.email_address, linkResolver);
-    var hoursHeadingHtml = RichText.render(pageData.hours_heading, linkResolver);
+    var hoursHeadingText = RichText.asText(pageData.hours_heading, linkResolver);
     var hoursTextHtml = RichText.render(pageData.hours_text, linkResolver);
     return (
       <section className="key-info">
         <div className="container-sm">
           <div className="cols-2 no-gutters">
             <div>
-              { locationHeadingHtml }
+              { locationHeadingText && (
+                <h3>{ locationHeadingText }</h3>
+              )}
               <a href="https://www.google.com/maps/place/1201+N+La+Brea+Ave,+West+Hollywood,+CA+90038/@34.092668,-118.3466097,17z/data=!3m1!4b1!4m5!3m4!1s0x80c2bf2841e514b5:0xa8cc818cc9b76bf5!8m2!3d34.092668!4d-118.344421" target="_blank">
               { addressHtml }
               </a>
               <p>
-                Tel. <a href={`tel:${phoneHtml}`}>{phoneHtml}</a>
+                { phoneHtml && (
+                  <span>Tel. <a href={`tel:${phoneHtml}`}>{phoneHtml}</a></span>
+                )}
                 <br />
                 <a href={`mailto:${emailAddressHtml}`}>{emailAddressHtml}</a>
               </p>
               <hr className="show-mobile" />
             </div>
             <div>
-            { hoursHeadingHtml }
+            { hoursHeadingText && (
+              <h3>{ hoursHeadingText }</h3>
+            )}
             { hoursTextHtml }
             </div>
           </div>
