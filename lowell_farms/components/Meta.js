9077d4c2e8beba323e78b58c9f6402bbff4e6e86
sarahholden <sarahbeth.holden@gmail.com>
2020-03-11 15:35:34 -0700

    Updates for NPM

--- lowell_farms/components/Meta.js
+++ lowell_farms/components/Meta.js
@@ -12,7 +12,7 @@ export default class Meta extends Component {
         <meta charSet="utf-8" />
         <link rel="shortcut icon" href="/static/favicon.png" />
         <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
-        <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+        <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
         <title>Lowell Herb Co</title>
         <meta
           name="google-site-verification"
