682814fb014dedbae5293c483b3fe271b45a0bb2
al3700 <aaron@funcsion.com>
2020-06-17 15:52:07 -0600

    Push Cannabis New

---
+++ lowell_farms/components/CannabisNew.js
@@ -0,0 +1,229 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable react/destructuring-assignment */
+import React, { Component } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import { RichText } from 'prismic-reactjs';
+import styled from 'styled-components';
+import Collection from './Collection';
+import ButtonLight from './styles/ButtonLight';
+import HeaderStyles from './styles/HeaderStyles';
+import Newsletter from './Newsletter';
+import Delivery from './Delivery';
+import DeliveryBanner from './DeliveryBanner';
+import Quote from './Quote';
+import { LazyLoadImage, LazyLoadComponent } from 'react-lazy-load-image-component';
+
+class Cannabis extends Component {
+  render() {
+    const {
+      pageData,
+      quotesData,
+      newsletterData,
+      products,
+      deliveryZipCodes,
+      zipCodeData,
+      stockists,
+      deliverySectionData,
+    } = this.props;
+    const heroBgImageUrl = pageData.hero_background_image.url;
+
+    const deliveryPlaceholderText =
+      deliverySectionData.delivery_placeholder != '' &&
+      RichText.asText(deliverySectionData.delivery_placeholder);
+    const deliveryButtonText =
+      deliverySectionData.delivery_button_text != '' &&
+      RichText.asText(deliverySectionData.delivery_button_text);
+
+    return (
+      <main>
+        <Head>
+
+          {pageData.meta_title != null && (
+            <title> {pageData.meta_title}</title>
+          )}
+          {pageData.meta_description != null && (
+            <meta name="description" content={pageData.meta_description} />
+          )}
+          {pageData.keywords != null && (
+            <meta name="keywords" content={pageData.keywords} />
+          )}
+          <link href="https://www.lowellfarms.com/cannabis" rel="canonical" />
+          {pageData.meta_title != null && (
+            <meta property="og:title" content={pageData.meta_title} />
+          )}
+          {pageData.meta_description != null && (
+            <meta property="og:description" content={pageData.meta_description} />
+          )}
+          {pageData.hero_background_image.url && (
+            <meta property="og:image" content={pageData.hero_background_image.url} />
+          )}
+        </Head>
+        <HeaderStyles
+          style={{
+            background: `#ecebe9 url(${heroBgImageUrl})`,
+            backgroundSize: 'cover',
+          }}
+        >
+          <h1>
+            {pageData.hero_heading != '' &&
+              RichText.asText(pageData.hero_heading)}
+          </h1>
+          <img
+            src={pageData.hero_image != '' && pageData.hero_image.url}
+            alt="Lowell Herb Co Cannabis"
+            width="200"
+            className="hero-image"
+          />
+        </HeaderStyles>
+        <Delivery
+          deliveryZipCodes={deliveryZipCodes}
+          pageData={deliverySectionData}
+          deliveryPlaceholderText={deliveryPlaceholderText}
+          deliveryButtonText={deliveryButtonText}
+          handle="Cannabis"
+          stockists={stockists}
+        />
+        <DeliveryBanner handle="Cannabis" zipCodeData={zipCodeData} />
+
+        <CalloutWrapper>
+
+          <section className="collection-overview">
+            {pageData.collection.map(collection => (
+              <section
+                key={
+                  collection.collection_handle &&
+                  RichText.asText(collection.collection_handle)
+                }
+              >
+                <div className="container">
+                  {collection.collection_heading != '' && (
+                    <h2>{RichText.asText(collection.collection_heading)}</h2>
+                  )}
+                  {collection.collection_text != '' && (
+                    <div className="intro-desc">
+                      {RichText.render(collection.collection_text)}
+                    </div>
+                  )}
+                  {collection.collection_url != '' && (
+                    <Link href={RichText.asText(collection.collection_url)}>
+                      <ButtonLight className="button">Shop Now</ButtonLight>
+                    </Link>
+                  )}
+                </div>
+                <Collection
+                  productCategory="Cannabis"
+                  handle={collection.collection_handle != '' && RichText.asText( collection.collection_handle)}
+                  products={products}
+                  deliveryZipCodes={deliveryZipCodes}
+                  limit={60}
+                  zipCodeData={zipCodeData}
+                />
+              </section>
+            ))}
+
+
+          </section>
+
+          <div className="quote">
+            <Quote quotesData={quotesData} index={1} styleClass="last-quote" />
+          </div>
+        </CalloutWrapper>
+
+        <Newsletter newsletterData={newsletterData} page="cannabis" />
+      </main>
+    );
+  }
+}
+
+export default Cannabis;
+
+const CalloutWrapper = styled.div`
+  background-color: #ecebe9;
+  .quote {
+    font-family: 'Lora Regular';
+  }
+  .col-right {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  .learn-more {
+    padding-top: 100px;
+  }
+  .about-text-col {
+    text-align: center;
+    .quote {
+      font-size: 2.2rem;
+      color: #333639;
+    }
+    .publication {
+      color: #6b6653;
+      font-size: 1.2rem;
+      margin-top: 0;
+      margin-bottom: 30px;
+    }
+  }
+
+  .collection-overview section {
+    text-align: center;
+    padding-top: 100px;
+    padding-bottom: 160px;
+    h2 {
+      font-family: 'Lora Regular';
+      font-size: 5rem;
+      color: #6B6653;
+      margin-bottom: 14px;
+      margin-top: 0;
+    }
+    .intro-desc {
+      font-family: 'Lora Regular';
+      color: #333639;
+      font-size: 1.8rem;
+      letter-spacing: .08rem;
+      p {
+        margin: 0;
+      }
+    }
+    &:nth-of-type(even) {
+      background: #E1DDDB;
+    }
+    &:last-of-type {
+      padding-bottom: 60px;
+    }
+    .button {
+      margin-top: 50px;
+      margin-bottom: 100px;
+    }
+  }
+
+  @media (min-width: 768px) {
+    .about-text-col {
+      max-width: 420px;
+      margin-right: 8%;
+      margin-left: auto;
+      text-align: left;
+    }
+  }
+  @media (max-width: 768px) {
+    padding-bottom: 80px;
+    .collection-overview section {
+      padding-top: 80px;
+      padding-bottom: 90px;
+      h2 {
+        font-size: 3.5rem;
+      }
+      .intro-desc {
+        font-size: 1.6rem;
+      }
+      .button {
+        margin-top: 40px;
+        margin-bottom:80px;
+      }
+      &:last-of-type {
+        padding-bottom: 0;
+      }
+    }
+  }
+`;
---
+++ lowell_farms/pages/cannabisnew.js
@@ -0,0 +1,59 @@
+/* eslint-disable react/prop-types */
+import React, { Component } from 'react';
+
+import { getPageApi, getPageByType } from '../api';
+import CannabisPage from '../components/CannabisNew';
+
+export default class BlogPost extends Component {
+  static async getInitialProps() {
+    // Query the Prismic API with the ID of the page
+    const pageId = 'XespnxEAAD1dW3hU';
+    const response = await getPageApi(pageId);
+
+    const productsResponse = await getPageByType('product');
+
+    // Query the Prismic API with the ID of the page
+    const deliverySectionId = 'XeslNREAABpeW2S5';
+    const deliverySectionResponse = await getPageApi(deliverySectionId);
+
+    const stockistsId = 'Xev6bxEAABBnXxL4';
+    const stockistsResponse = await getPageApi(stockistsId);
+
+    return {
+      page: response,
+      products: productsResponse,
+      stockists: stockistsResponse,
+      deliverySectionContent: deliverySectionResponse,
+    };
+  }
+
+  render() {
+    const {
+      page,
+      newsletter,
+      quotes,
+      products,
+      zipCodes,
+      stockists,
+      deliverySectionContent,
+    } = this.props;
+    const { data } = page;
+
+    const cannabisDeliveryZones = zipCodes.cannabis_delivery_zones.map(
+      zipCodeItem => zipCodeItem.zip_code
+    );
+
+    return (
+      <CannabisPage
+        pageData={data}
+        newsletterData={newsletter}
+        quotesData={quotes}
+        products={products}
+        deliveryZipCodes={cannabisDeliveryZones}
+        zipCodeData={zipCodes}
+        stockists={stockists.data}
+        deliverySectionData={deliverySectionContent.data}
+      />
+    );
+  }
+}
